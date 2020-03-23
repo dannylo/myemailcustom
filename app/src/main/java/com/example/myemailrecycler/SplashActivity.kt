14a507b20e3f4af27c51e4fce8b8a9e871704f3a
Dannylo Johnathan <dannylojohnathan@gmail.com>
2020-03-23 15:10:00 -0300

    splash screen

---
+++ app/src/main/java/com/example/myemailrecycler/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myemailrecycler
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+
+}
